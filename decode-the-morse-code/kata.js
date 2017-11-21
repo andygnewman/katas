e4675be1cd1768cf36767d3d9290a66c61d32479
Andy <andygnewman@gmail.com>
2017-11-21 14:21:28 +0000

    solution for bits

--- decode-the-morse-code/kata.js
+++ decode-the-morse-code/kata.js
@@ -1,7 +1,14 @@
 const bits = '1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011';
 
+// Not particularly happy with this solution
+// Key issue is how to differentiate '111' with no other data
+// Tests assume it is a dash, whereas it could be a dot with a timecode of three
+// With no '0's or other '1's it is hard to tell
 const decodeBits = (bits) => {
-  const time = bits.split(/1+/).filter(a => a.length > 0).sort().shift().length;
+  bits = bits.replace(/^0+|0+$/g, ''); // trim leading & following 0's
+  const timeFrom1 = bits.match(/1+/g) && bits.match(/1+/g).sort().shift().length; // shortest group of 1's
+  const timeFrom0 = bits.match(/0+/g) && bits.match(/0+/g).sort().shift().length; // shortest group of 0's
+  const time = Math.min(timeFrom1, timeFrom0 || timeFrom1); // shortest overall - assume this is the time
   const timeNormalized = bits.split('').map((bit, index) => (index + 1) % time === 0 ? bit : '').join('');
   return timeNormalized
     .replace(/1{3}/g, '-') // convert 3 1's to dashes
@@ -17,4 +24,4 @@ const decodeMorse = (morseCode) => {
   }).join('');
 };
 
-console.log(decodeBits(bits));
+console.log(decodeMorse(decodeBits(bits)));
