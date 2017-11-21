7d0daa45b2995c9b0853e6dd2ba3b07f6a00969b
Andy <andygnewman@gmail.com>
2017-11-21 15:05:41 +0000

    tidy up commenting

--- pagination-helper/kata.js
+++ pagination-helper/kata.js
@@ -1,24 +1,4 @@
-// // TODO: complete this object/class
-//
-// function PaginationHelper(collection, itemsPerPage){
-//
-// }
-//
-// PaginationHelper.prototype.itemCount = function() {
-//
-// }
-//
-// PaginationHelper.prototype.pageCount = function() {
-//
-// }
-//
-// PaginationHelper.prototype.pageItemCount = function(pageIndex) {
-//
-// }
-//
-// PaginationHelper.prototype.pageIndex = function(itemIndex) {
-//
-// }
+// complete this object/class
 
 class PaginationHelper {
   // The constructor takes in an array of items and a integer indicating how many
@@ -54,17 +34,3 @@ class PaginationHelper {
     return parseInt((itemIndex + 1) / this.itemsPerPage);
   }
 }
-
-
-const helper = new PaginationHelper(['a','b','c','d','e','f'], 4);
-console.log('pageCount: ', helper.pageCount()); //should == 2
-console.log('itemsCount: ', helper.itemCount()); //should == 6
-helper.pageItemCount(0); //should == 4
-helper.pageItemCount(1); // last page - should == 2
-helper.pageItemCount(2); // should == -1 since the page is invalid
-
-// pageIndex takes an item index and returns the page that it belongs on
-helper.pageIndex(5); //should == 1 (zero based index)
-helper.pageIndex(2); //should == 0
-helper.pageIndex(20); //should == -1
-helper.pageIndex(-10); //should == -1
